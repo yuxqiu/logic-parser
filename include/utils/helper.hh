39718f488c80a3df7dd1ee0873e2c6e79f62724e
Yuxiang Qiu <qyxtim@qq.com>
2023-04-19 00:23:40 +0100

    fix: fix helper

--- include/utils/helper.hh
+++ include/utils/helper.hh
@@ -1,5 +1,7 @@
+#pragma once
+
 #ifdef __GNUC__ // GCC, Clang, ICC
 #define unreachable() __builtin_unreachable()
-#elifdef _MSC_VER // MSVC
+#elif defined(_MSC_VER) // MSVC
 #define unreachable() __assume(false)
 #endif
